7e2bd9ce75d01a23112c88924d3620888fccf0dd
VeronikaRevjakina <veronikaw777@gmail.com>
2020-03-29 13:52:06 +0300

    add series for all pk and modified due to comments

--- database/quest_labyrinth2.sql
+++ database/quest_labyrinth2.sql
@@ -5,80 +5,126 @@ CREATE DATABASE "quest_labyrinth" WITH ENCODING = 'UTF8';
 --\c "postgres"
 
 
-
-
 CREATE TABLE actions_for_state (
-    id smallint NOT NULL,
-    coordinate_x smallint NOT NULL,
-    coordinate_y smallint NOT NULL,
+    id integer NOT NULL,
+    coordinate_x bigint NOT NULL,
+    coordinate_y bigint NOT NULL,
     possible_action_id integer NOT NULL
 );
 
 
 
+
+CREATE SEQUENCE actions_for_state_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
 CREATE TABLE actions_id (
-    id smallint NOT NULL,
     action_id integer NOT NULL,
     action_name character varying(50) NOT NULL
 );
 
 
 
+CREATE SEQUENCE actions_id_action_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
 CREATE TABLE screenplay_for_state (
-    id smallint NOT NULL,
-    coordinate_x smallint NOT NULL,
-    coordinate_y smallint NOT NULL,
+    id integer NOT NULL,
+    coordinate_x bigint NOT NULL,
+    coordinate_y bigint NOT NULL,
     current_screenplay_part_id smallint NOT NULL
 );
 
 
 
+CREATE SEQUENCE screenplay_for_state_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
 CREATE TABLE screenplay_id (
-    id smallint NOT NULL,
     screenplay_part_id integer NOT NULL,
     screenplay_part_text character varying(50) NOT NULL
 );
 
 
 
+CREATE SEQUENCE screenplay_id_screenplay_part_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
 CREATE TABLE user_inventory (
-    id smallint NOT NULL,
+    id integer NOT NULL,
     user_id integer NOT NULL,
     item_name character varying(50) NOT NULL
 );
 
 
 
+CREATE SEQUENCE user_inventory_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+
 CREATE TABLE user_state (
-    id smallint NOT NULL,
     user_id integer NOT NULL,
     user_name character varying(50) NOT NULL,
-    coordinate_x smallint NOT NULL,
-    coordinate_y smallint NOT NULL,
-    time_before_attack smallint NOT NULL
+    coordinate_x bigint NOT NULL,
+    coordinate_y bigint NOT NULL,
+    time_before_attack bigint NOT NULL
 );
 
 
 
+CREATE SEQUENCE user_state_user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
-ALTER TABLE ONLY actions_for_state
-    ADD CONSTRAINT actions_for_state_pkey PRIMARY KEY (id);
 
 
 ALTER TABLE ONLY actions_id
-    ADD CONSTRAINT actions_id_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT actions_id_pkey PRIMARY KEY (action_id);
 
 
-ALTER TABLE ONLY screenplay_for_state
-    ADD CONSTRAINT screenplay_for_state_current_screenplay_part_id_key UNIQUE (current_screenplay_part_id);
 
 
 ALTER TABLE ONLY screenplay_for_state
     ADD CONSTRAINT screenplay_for_state_pkey PRIMARY KEY (id);
 
 
+
+
 ALTER TABLE ONLY screenplay_id
-    ADD CONSTRAINT screenplay_id_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT screenplay_id_pkey PRIMARY KEY (screenplay_part_id);
+
 
 
 
@@ -87,7 +133,9 @@ ALTER TABLE ONLY user_inventory
 
 
 
+
 ALTER TABLE ONLY user_state
-    ADD CONSTRAINT user_state_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT user_state_pkey PRIMARY KEY (user_id);
+
 
 
