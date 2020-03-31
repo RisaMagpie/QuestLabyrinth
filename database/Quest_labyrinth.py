30cc42cbfb606099cf63dd82f9099eca29e705ca
RisaMagpie <risahawkeye@mail.ru>
2020-03-31 22:06:51 +0300

    Update Quest_labyrinth.py

--- database/Quest_labyrinth.py
+++ database/Quest_labyrinth.py
@@ -14,8 +14,7 @@ try:
     conn.set_isolation_level(0)
     conn.autocommit = True 
 except:
-    print("Unable to connect to the database."
-)
+    print("Unable to connect to the database.")
 cur = conn.cursor()
 
 try:
@@ -28,9 +27,6 @@ try:
 #     conn.close()
 except:
     print ("Error: ", sys.exc_info()[1])
-
-
-# In[34]:
 
 
 #cur.execute('''CREATE DATABASE "quest_labyrinth2" WITH ENCODING = 'UTF8';''') 
