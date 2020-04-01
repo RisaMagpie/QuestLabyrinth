a2065c4f40ba11704861dd47d81703e1f68aa806
Svetlana <sveta.soroka1997@gmail.com>
2020-04-01 18:20:18 +0300

    fix imports

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import re
 import telegram
 from telebot.credentials import bot_token, bot_user_name, URL
+import re
 from telebot import user_register
 
 global bot
