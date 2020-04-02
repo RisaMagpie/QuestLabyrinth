a63ce82f9783707c511e390eec5cc5c3eccc5467
Svetlana <sveta.soroka1997@gmail.com>
2020-04-02 14:28:28 +0300

    add non-manual webhook

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
-
+set_webhook()
 
 #for routes read here: https://flask.palletsprojects.com/en/1.0.x/quickstart/
 @app.route('/{}'.format(TOKEN), methods=['POST'])
