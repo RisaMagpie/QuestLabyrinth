ca138cfc0741564fa8eb84505eaefa13334b3b1a
Svetlana <sveta.soroka1997@gmail.com>
2020-04-08 15:20:31 +0300

    reg files

---
+++ telebot/database/user_register_queries.py
@@ -0,0 +1,94 @@
+import os
+import psycopg2
+
+def check_user_existance(user_telegram_id:int) -> bool:
+    """  
+    This function checks existance of a user with user_telegram_id in the database.
+    """
+    is_user_exists:bool = False
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        conn.set_isolation_level(0)
+        conn.autocommit = True
+        cur = conn.cursor()        
+    except:
+        print("Unable to connect to the database.")        
+    if cur:    
+        try:
+            cur.execute("""
+            SELECT user_id
+            FROM user_state
+            WHERE user_id = %s
+            """, (user_telegram_id, ))
+            records = cur.fetchall()
+            if len(records)>0:
+                is_user_exists = True                
+        except:
+            print("Can\'t to execute check_user_existance query") 
+        cur.close()
+        conn.close()
+    return is_user_exists
+    
+def set_zero_user_state(user_telegram_id:int) -> bool:
+    """  
+    This function set user coordinates to zero and monster counter to init value.
+    """
+    updated_sucessful:bool = False
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        conn.set_isolation_level(0)
+        conn.autocommit = True
+        cur = conn.cursor()        
+    except:
+        print("Unable to connect to the database.")
+        
+    if cur:    
+        try:
+            cur.execute("""
+            UPDATE user_state 
+            SET  coordinate_x = 1, 
+            coordinate_y = 0, 
+            time_before_attack = 1000 
+            WHERE user_id = %s;
+            """, (user_telegram_id, ))
+            updated_sucessful = bool(cur.rowcount)
+            conn.commit()             
+        except:
+            print("Can\'t to execute set_zero_user_state query") 
+        cur.close()
+        conn.close()
+    return updated_sucessful
+    
+def create_new_user(user_telegram_id:int) -> bool:
+    """  
+    This function create a new user row and
+    set user coordinates to zero and monster counter to default value.
+    """
+    created_successful:bool = False
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        conn.set_isolation_level(0)
+        conn.autocommit = True
+        cur = conn.cursor()        
+    except:
+        print("Unable to connect to the database.")
+        
+    if cur:    
+        try:
+            cur.execute("""
+            INSERT INTO user_state (user_id, 
+            coordinate_x, 
+            coordinate_y, 
+            time_before_attack) VALUES (%s, 0, 0, 1000)
+            """, (user_telegram_id, ))
+            conn.commit()          
+            created_successful:bool = True
+        except:
+            print("Can\'t to execute create_new_user query") 
+        cur.close()
+        conn.close()
+    return created_successful    
+    
---
+++ telebot/user_register.py
@@ -0,0 +1,23 @@
+from .database import user_register_queries
+
+
+def registration(user_telegram_id:int) -> bool:
+    """
+    Before creation of a new user we need to check his existance in the database.
+    If user exists we need to update his state to init state.
+    Otherwise we need to create a new user with init state in a user_state table.
+    After that we need to return from database text message and possible actions.
+    """
+    is_exist:bool = user_register_queries.check_user_existance(user_telegram_id)
+    
+    if is_exist:
+        is_created:bool = user_register_queries.set_zero_user_state(user_telegram_id)
+    else:
+        is_created:bool = user_register_queries.create_new_user(user_telegram_id)
+            
+    # !!!        
+    # After that we need to return from database text message and possible actions.
+    # But now database is empty.
+ 
+    return is_created
+    
