1f091004a9022b1110cf4aad9ad4d080db5ca8d2
Svetlana <sveta.soroka1997@gmail.com>
2020-04-18 21:13:18 +0300

    change scheme

--- telebot/database/database.py
+++ telebot/database/database.py
@@ -23,42 +23,46 @@ def create_database():
             cur.execute("DROP TABLE IF EXISTS " + row[1] + " cascade")
             print("dropping table: ", row[1])
 
-        cur.execute('''CREATE TABLE  actions_for_state (
-            id SERIAL PRIMARY KEY,
-            coordinate_x BIGINT NOT NULL,
-            coordinate_y BIGINT NOT NULL,
-            possible_action_id integer NOT NULL
-        );''')
-
         cur.execute('''CREATE TABLE actions_id (
             action_id SERIAL PRIMARY KEY,
             action_name character varying(50) NOT NULL
+            UNIQUE (action_name)
         );''')
 
         cur.execute('''CREATE TABLE screenplay_for_state (
             id SERIAL PRIMARY KEY,
-            coordinate_x BIGINT NOT NULL DEFAULT 0,
-            coordinate_y BIGINT NOT NULL DEFAULT 0,
-            current_screenplay_part_id smallint NOT NULL
-        );''')
+            coordinate_x BIGINT NOT NULL,
+            coordinate_y BIGINT NOT NULL,
+            current_screenplay_part_id smallint NOT NULL,
+            UNIQUE(coordinate_x, coordinate_y, current_screenplay_part_id)
+        );''') #screenplay 1:m coordinates, actions m:m 
+        
+        cur.execute('''CREATE TABLE actions_for_state (
+            id SERIAL PRIMARY KEY,
+            coordinate_x BIGINT NOT NULL,
+            coordinate_y BIGINT NOT NULL,
+            possible_action_id integer NOT NULL,
+            UNIQUE(coordinate_x, coordinate_y, possible_action_id)            
+        );''') #screenplay 1:m coordinates, actions m:m 
 
         cur.execute('''CREATE TABLE screenplay_id (
             screenplay_part_id SERIAL PRIMARY KEY,
-            screenplay_part_text character varying(50) NOT NULL
+            screenplay_part_text character varying(50) NOT NULL,
+            UNIQUE (screenplay_part_text)
         );''')
 
         cur.execute('''CREATE TABLE user_inventory (
             id SERIAL PRIMARY KEY,
             user_id integer NOT NULL,
-            item_name character varying(50) NOT NULL
+            item_name character varying(50) NOT NULL,
+            UNIQUE(user_id, item_name)
         );''')
 
         cur.execute('''CREATE TABLE user_state (
             user_id BIGINT PRIMARY KEY,
-            coordinate_x BIGINT NOT NULL,
-            coordinate_y BIGINT NOT NULL,
-            time_before_attack BIGINT NOT NULL,
-            UNIQUE (coordinate_x,coordinate_y)
+            coordinate_x BIGINT NOT NULL DEFAULT 0,
+            coordinate_y BIGINT NOT NULL DEFAULT 0,
+            time_before_attack BIGINT NOT NULL DEFAULT 1000
         );''')
 
         cur.execute('''ALTER TABLE actions_for_state 
@@ -66,29 +70,29 @@ def create_database():
                     FOREIGN KEY (possible_action_id)
                     REFERENCES actions_id(action_id) 
                     ON DELETE CASCADE ON UPDATE CASCADE;''')
-
-        cur.execute('''ALTER TABLE screenplay_for_state 
-                    ADD CONSTRAINT coordinate_screenplay_fk
+        
+        cur.execute('''ALTER TABLE actions_for_state 
+                    ADD CONSTRAINT action_coordinates_id_fk
                     FOREIGN KEY (coordinate_x,coordinate_y)
-                    REFERENCES user_state(coordinate_x,coordinate_y) 
+                    REFERENCES screenplay_for_state(coordinate_x,coordinate_y) 
+                    ON DELETE CASCADE ON UPDATE CASCADE;''')
+
+        cur.execute('''ALTER TABLE user_state 
+                    ADD CONSTRAINT user_coordinates_fk
+                    FOREIGN KEY (coordinate_x,coordinate_y)
+                    REFERENCES screenplay_for_state(coordinate_x,coordinate_y) 
                     ON DELETE CASCADE ON UPDATE CASCADE;''')
 
         cur.execute('''ALTER TABLE screenplay_for_state 
                     ADD CONSTRAINT current_screenplay_part_id_fk
                     FOREIGN KEY (current_screenplay_part_id)
-                    REFERENCES screenplay_id 
+                    REFERENCES screenplay_id(screenplay_part_id)
                     ON DELETE CASCADE ON UPDATE CASCADE;''')
 
         cur.execute('''ALTER TABLE user_inventory 
                     ADD CONSTRAINT user_id_fk
                     FOREIGN KEY (user_id)
                     REFERENCES user_state(user_id) 
-                    ON DELETE CASCADE ON UPDATE CASCADE;''')
-        
-        cur.execute('''ALTER TABLE actions_for_state
-                    ADD CONSTRAINT coordinate_action_fk
-                    FOREIGN KEY (coordinate_x,coordinate_y)
-                    REFERENCES user_state(coordinate_x,coordinate_y) 
                     ON DELETE CASCADE ON UPDATE CASCADE;''')
 
         print("Table created successfully")
