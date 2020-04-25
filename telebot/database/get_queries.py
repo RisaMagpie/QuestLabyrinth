4dd5576d77dc3168b8a43ec6fc5f2d7c4988db59
VeronikaRevjakina <veronikaw777@gmail.com>
2020-04-25 15:23:32 +0300

    added select screenplay text, deleted main

--- telebot/database/get_queries.py
+++ telebot/database/get_queries.py
@@ -35,6 +35,30 @@ def get_possible_actions_text(coordinate_x:int,coordinate_y:int)->[str]:
         conn.close()
     return result
 
-if __name__ == "__main__":
-    print(get_possible_actions_text(0,0))
+
+def get_screenplay_part_text(coordinate_x:int,coordinate_y:int)->[str]:
+    """Get screenplay text by coordinates."""
+    try:
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        conn.set_isolation_level(0)
+        conn.autocommit = True
+        cur = conn.cursor()        
+    except:
+        print("Unable to connect to the database.")   
+    if cur:    
+        try:
+            cur.execute("""
+            SELECT screenplay_part_text 
+            FROM screenplay_id 
+            INNER JOIN screenplay_for_state 
+            ON screenplay_part_id = current_screenplay_part_id
+            WHERE coordinate_x= %s AND coordinate_y= %s;
+            """, (coordinate_x,coordinate_y))
+            result = [item[0] for item in cur.fetchall()]
+
+        except:
+            print("Can\'t to execute get_user_state query") 
+        cur.close()
+        conn.close()
+    return result
 
